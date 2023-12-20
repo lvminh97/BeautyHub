497cbee2d218781a9f6bf509bb95de2ae0d248f0
lvminh97 <lvminh97@gmail.com>
2023-12-20 16:22:43 +0700

    First commit

---
+++ beauty_hub_admin/lib/modules/order_manage/view/order_manage_page.dart
@@ -0,0 +1,16 @@
+import 'package:beauty_hub_admin/modules/order_manage/controller/order_manage_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class OrderManagepage extends GetView<OrderManageController> {
+  const OrderManagepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
---
+++ beauty_hub_admin/lib/modules/revenue_statistics/view/revenue_statisitics_page.dart
@@ -0,0 +1,16 @@
+import 'package:beauty_hub_admin/modules/revenue_statistics/controller/revenue_statistics_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class RevenueStatisticsPage extends GetView<RevenueStatisticsController> {
+  const RevenueStatisticsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
